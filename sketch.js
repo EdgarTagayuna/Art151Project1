6e76429f14ebae0cbd876002c8e09b554b4429c4
Grey Luvs <etagay2@uic.edu>
2021-09-01 10:40:11 -0500

    Target Lessons

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
   }
   
+  n = 50
+  accuracy = 10
+
   function draw() {
-    background(0,225,220);
+    background(mouseX,mouseY,n);
+  
+    fill(300,300,300)
+    circle(mouseX,mouseY,n)
+    
+    line(mouseX-(n/2),mouseY,mouseX+(n/2),mouseY)
+    line(mouseX,mouseY-(n/2),mouseX,mouseY+(n/2))
+    
+    if (n> 255 || n < 50){
+      accuracy = accuracy*-1
+    }
+    
+    n = n + accuracy
   }
